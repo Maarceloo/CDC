dc19425f4b3d41f81969807133aeffd01e653537
Marcelo De Lima <delima_marcelo@outlook.com>
2023-03-15 20:15:37 -0300

    funcao para acesso ao backend

---
+++ frontend/src/service/request.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: "http://localhost:3001",
+});
+
+export const getData = async (endpoint) => {
+  const { data } = await request.get(endpoint);
+  return data;
+};
+
+export const postData = async (endpoint, body) => {
+  const { data } = await request.post(endpoint, body);
+  return data;
+};
+
+export default request;
